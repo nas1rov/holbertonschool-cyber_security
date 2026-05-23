b82361769adf05ef86446ede512c6fca2fb91461
Kamran Nasirov <13778@holbertonstudents.com>
2026-05-23 23:19:46 +0400

    1-dec2bin.sh

--- network_security/2x00_network_fundamentals/1-dec2bin.sh
+++ network_security/2x00_network_fundamentals/1-dec2bin.sh
@@ -1,17 +1,2 @@
 #!/bin/bash
-
-# Assign the first argument to a variable
-decimal=$1
-
-# Initialize an empty string for the binary output
-binary=""
-
-# Loop from bit position 7 down to 0
-for ((i=7; i>=0; i--)); do
-    # Right-shift the number by 'i' positions and check the last bit
-    bit=$(( (decimal >> i) & 1 ))
-    binary="${binary}${bit}"
-done
-
-# Output the final 8-bit string
-echo "$binary"
+printf "%08d\n" $(echo "obase=2; $1" | bc)
