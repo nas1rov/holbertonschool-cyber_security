6d31c7913ee359a251d2c85c02878b0b77a80980
Kamran Nasirov <13778@holbertonstudents.com>
2026-05-23 23:34:04 +0400

    6-broadcast.sh

---
+++ network_security/2x00_network_fundamentals/6-broadcast.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+IFS=. read -ra ip <<< "$1"; IFS=. read -ra mask <<< "$2"; printf "%d.%d.%d.%d\n" $(((ip[0] & mask[0]) | (~mask[0] & 255))) $(((ip[1] & mask[1]) | (~mask[1] & 255))) $(((ip[2] & mask[2]) | (~mask[2] & 255))) $(((ip[3] & mask[3]) | (~mask[3] & 255)))
